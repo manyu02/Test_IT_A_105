f195d6255e3d310bdc6659840c8ef8f57de0664d
manyu02 <139069119+manyu02@users.noreply.github.com>
2024-11-11 13:11:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-const username = document.querySelector('#username');
-const password = document.querySelector("#password");
-const button = document.querySelector(".login-btn");
-const login = document.querySelector("#login-container");
-const git = document.querySelector("#fetch-git");
-const main_body = document.getElementById("main-body");
-const gitlist = document.querySelector(".gitlist");
-
-const user = {
-    username: "admin",
-    password: "admin"
-};
-
-main_body.style.display = "none";
-
-button.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (username.value === user.username) {
-        if (password.value === user.password) {
-            login.style.display = "none";
-            main_body.style.display = "block";
-        } else {
-            alert("Wrong password");
-        }
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+    if (username=='Tapan' && password=='Tapan') {
+        alert("Login successful!");
+        window.location.href = "dash.html";
     } else {
-        alert("No user exists");
+        alert("Invalid Login Details");
     }
-});
-
-git.addEventListener("click", async () => {
-    let data = await fetch("https://api.github.com/users");
-    let jsonData = await data.json();
-
-    gitlist.innerHTML = "";
-    jsonData.slice(0, 10).forEach((user) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = user.login;
-        gitlist.appendChild(listItem);
-    });
 });
